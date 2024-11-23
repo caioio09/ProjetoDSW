a2ed9d12d05f21884cce0bac3dbaed963e39905c
Caio Santos <91094630+caioio09@users.noreply.github.com>
2024-11-23 00:57:40 -0300

    Inicio

---
+++ ProjetoIndividualDSW/src/main/java/com/caioAtividadeDSW/ProjetoIndividualDSW/ProjetoIndividualDswApplication.java
@@ -0,0 +1,13 @@
+package com.caioAtividadeDSW.ProjetoIndividualDSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIndividualDswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIndividualDswApplication.class, args);
+	}
+
+}
---
+++ ProjetoIndividualDSW/src/test/java/com/caioAtividadeDSW/ProjetoIndividualDSW/ProjetoIndividualDswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caioAtividadeDSW.ProjetoIndividualDSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIndividualDswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
